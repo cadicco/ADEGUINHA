04bfcd70e4c665d03528884377a27d2dde13c739
cadicco <cadicco@gmail.com>
2026-03-17 23:14:41 -0300

    Alteração necessária 2

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username_db = "root"; // Usuário padrão do XAMPP
 $password_db = "";     // Senha padrão do XAMPP
-$dbname = "adega_db";
+$dbname = "adeguinha";
 
 // Criar conexão
 $conn = new mysqli($servername, $username_db, $password_db, $dbname);
